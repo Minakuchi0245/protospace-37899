0c2e41a65f89505ffb4fba05c8e88f3424950879
Minakuchi0245 <kamilia3791@gmail.com>
2022-05-11 14:23:38 +0900

    first commit

---
+++ db/migrate/20220510063244_create_coments.rb
@@ -0,0 +1,10 @@
+class CreateComents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :coments do |t|
+      t.text       :content,             null: false
+      t.references :user,                null: false, foreign_key: true
+      t.references :prototype,           null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
